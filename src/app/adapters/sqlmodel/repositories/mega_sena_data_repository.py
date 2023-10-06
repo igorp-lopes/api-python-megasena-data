b6fc8e0dffc6f676db5fb967b5012ffcd4503407
igorplopes <igorp.lopes16@gmail.com>
2023-10-05 22:31:16 -0300

    feat: add basic mega sena data sqlmodel repository

--- src/app/adapters/sqlmodel/repositories/mega_sena_data_repository.py
+++ src/app/adapters/sqlmodel/repositories/mega_sena_data_repository.py
@@ -1,8 +1,28 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncEngine
+from typing import Iterable
 
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from src.app.adapters.sqlmodel.entities.mega_sena_data import SqlModelMegaSenaData
 from src.app.ports.mega_sena_record_repository_port import MegaSenaRecordRepositoryPort
 
 
 class MegaSenaDataRepository(MegaSenaRecordRepositoryPort):
     def __init__(self, engine: AsyncEngine):
-        self.db = async_sessionmaker(engine, expire_on_commit=False)
+        self.session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+    async def save_record(self, mega_sena_record: SqlModelMegaSenaData):
+        async with self.session() as session:
+            session.add(mega_sena_record)
+            await session.commit()
+            await session.refresh(mega_sena_record)
+            return mega_sena_record
+
+    async def save_many_records(self, mega_sena_records: Iterable[SqlModelMegaSenaData]):
+        async with self.session() as session:
+            session.add_all(mega_sena_records)
+            await session.commit()
+            await session.refresh(mega_sena_records)
+            return mega_sena_records
--- src/app/core/container.py
+++ src/app/core/container.py
@@ -1,11 +1,20 @@
 from dependency_injector import containers, providers
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel import create_engine
 
+from src.app.adapters.sqlmodel.repositories.mega_sena_data_repository import MegaSenaDataRepository
+from src.app.core.config import DATABASE_URL
 from src.app.services.mega_sena_scrapper_service import MegaSenaScrapperService
 
 
 class Container(containers.DeclarativeContainer):
+
+    db_engine = AsyncEngine(create_engine(DATABASE_URL, echo=True, future=True))
+
     wiring_config = containers.WiringConfiguration(
         modules=["src.app.controllers.health_controller",
                  "src.cron_job.test_cron"])
 
     mega_sena_scrapper_service = providers.Factory(MegaSenaScrapperService)
+
+    mega_sena_data_repository = providers.Factory(MegaSenaDataRepository, engine=db_engine)
